8559fa49de21bc1247ba3feac745bbae1bc15276
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-02-09 19:28:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# FarmingTimer
+
+<p align="center">
+  <a href="https://github.com/VermeilChan/FarmingTimer">
+    <img src="Assets/readme.png" alt="Stupid Car">
+  </a>
+</p>
+
+<p align="center">
+  <a href="https://github.com/VermeilChan/FarmingTimer/graphs/contributors">
+    <img alt="Contributors" src="https://img.shields.io/github/contributors/VermeilChan/FarmingTimer" />
+  </a>
+  <a href="https://github.com/VermeilChan/FarmingTimer/releases">
+    <img alt="Latest Release" src="https://img.shields.io/github/release/VermeilChan/FarmingTimer" />
+  </a>
+  <a href="https://github.com/VermeilChan/FarmingTimer/releases">
+    <img alt="Downloads" src="https://img.shields.io/github/downloads/VermeilChan/FarmingTimer/total" />
+  </a>
+  <a href="https://github.com/VermeilChan/FarmingTimer/LICENSE">
+    <img alt="License" src="https://img.shields.io/github/license/VermeilChan/FarmingTimer" />
+  </a>
+  <a href="https://github.com/VermeilChan/FarmingTimer/issues">
+    <img alt="Open Issues" src="https://img.shields.io/github/issues/VermeilChan/FarmingTimer" />
+  </a>
+  <a href="https://github.com/VermeilChan/FarmingTimer/pulls">
+    <img alt="Open Pull Requests" src="https://img.shields.io/github/issues-pr/VermeilChan/FarmingTimer" />
+  </a>
+  <a href="https://github.com/VermeilChan/FarmingTimer/commits/main">
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/VermeilChan/FarmingTimer" />
+  </a>
+    <img alt="GitHub forks" src="https://img.shields.io/github/forks/VermeilChan/FarmingTimer" />
+  </a>
+    <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/VermeilChan/FarmingTimer" />
+  </a>
+</p>
+
+If you have a GitHub account and have found this repository helpful, please consider starring ★ it.
+
+## Table of contents
+
+- [Overview](#overview)
+- [Features](#features)
+- [License](#license)
+- [Credits](#credits)
+
+## Overview
+
+FarmingTimer is a straightforward program designed to track your farming time. Its goal is to keep things simple, easy to use, and lightweight.
+
+## Features
+
+- Start, pause, resume, and stop timer functionality.
+- easy-to-use user interface & lightweight.
+
+## License
+
+This project is licensed under the [GNU General Public License v3.0](LICENSE).
+
+## Credits
+
+- [PySide6](https://doc.qt.io/qtforpython-6/#)
---
+++ Source/main.py
@@ -0,0 +1,94 @@
+import sys
+from PySide6.QtCore import QTimer, Qt
+from PySide6.QtGui import QIcon, QFont
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel
+
+class FarmingTimer(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Farming Timer")
+        self.setWindowIcon(QIcon('Assets/Icon/icon.ico'))
+        self.setGeometry(100, 100, 300, 200)
+
+        self.timer_label = QLabel("00:00:00")
+        font = QFont("Arial", 24)
+        self.timer_label.setFont(font)
+
+        self.start_button = QPushButton(QIcon('Assets/buttons/start_icon.svg'), "Start")
+        self.pause_button = QPushButton(QIcon('Assets/buttons/pause_icon.svg'), "Pause")
+        self.stop_button = QPushButton(QIcon('Assets/buttons/stop_icon.svg'), "Stop")
+        self.resume_button = QPushButton(QIcon('Assets/buttons/resume_icon.svg'), "Resume")
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.timer_label, alignment=Qt.AlignCenter)
+        
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.start_button)
+        button_layout.addWidget(self.pause_button)
+        button_layout.addWidget(self.stop_button)
+        button_layout.addWidget(self.resume_button)
+
+        self.layout.addLayout(button_layout)
+        self.setLayout(self.layout)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_timer)
+        self.time_elapsed = 0
+
+        self.start_button.clicked.connect(self.start_timer)
+        self.pause_button.clicked.connect(self.pause_timer)
+        self.stop_button.clicked.connect(self.stop_timer)
+        self.resume_button.clicked.connect(self.resume_timer)
+
+        self.pause_button.setEnabled(False)
+        self.stop_button.setEnabled(False)
+        self.resume_button.setEnabled(False)
+
+        self.timer_running = False
+
+    def start_timer(self):
+        self.timer.start(1000)
+        self.start_button.setEnabled(False)
+        self.pause_button.setEnabled(True)
+        self.stop_button.setEnabled(True)
+        self.resume_button.setEnabled(False)
+        self.timer_running = True
+
+    def pause_timer(self):
+        self.timer.stop()
+        self.pause_button.setEnabled(False)
+        self.resume_button.setEnabled(True)
+        self.timer_running = False
+
+    def stop_timer(self):
+        self.timer.stop()
+        self.start_button.setEnabled(True)
+        self.pause_button.setEnabled(False)
+        self.stop_button.setEnabled(False)
+        self.resume_button.setEnabled(False)
+        self.time_elapsed = 0
+        self.update_timer()
+        self.timer_running = False
+
+    def resume_timer(self):
+        self.timer.start(1000)
+        self.pause_button.setEnabled(True)
+        self.resume_button.setEnabled(False)
+        self.timer_running = True
+
+    def update_timer(self):
+        if self.timer_running:
+            self.time_elapsed += 1
+            hours = self.time_elapsed // 3600
+            minutes = (self.time_elapsed % 3600) // 60
+            seconds = self.time_elapsed % 60
+            self.timer_label.setText(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    timer_window = FarmingTimer()
+    timer_window.show()
+    sys.exit(app.exec())
